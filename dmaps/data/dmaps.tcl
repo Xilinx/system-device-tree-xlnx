1840dc3c1a07125361f4ceef0b0215ef67cbd9fd
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2019-11-28 01:38:50 -0700

    Add properties xlnx-dma-type and xlnx,clock-freq
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- dmaps/data/dmaps.tcl
+++ dmaps/data/dmaps.tcl
@@ -13,10 +13,27 @@
 #
 
 proc generate {drv_handle} {
+
+    foreach i [get_sw_cores device_tree] {
+        set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+        if {[file exists $common_tcl_file]} {
+            source $common_tcl_file
+            break
+        }
+    }
+
     set ip [get_cells -hier $drv_handle]
 
     #disabling non-secure dma
     if { [string match -nocase $ip "ps7_dma_ns"] } {
         set_property NAME none $drv_handle
+    }
+    set ip_name [get_property IP_NAME [get_cells -hier $drv_handle]]
+    set req_dma_list "psu_gdma psu_adma psu_csudma"
+    if {[lsearch  -nocase $req_dma_list $ip_name] >= 0} {
+        set_drv_conf_prop $drv_handle C_DMA_MODE xlnx,dma-type int
+	if {[string match -nocase $ip_name "psu_csudma"]} {
+	    hsi::utils::add_new_property $drv_handle "xlnx,dma-type" int 0
+        }
     }
 }
--- iicps/data/iicps.tcl
+++ iicps/data/iicps.tcl
@@ -22,4 +22,6 @@ proc generate {drv_handle} {
     }
 
     ps7_reset_handle $drv_handle CONFIG.C_I2C_RESET CONFIG.i2c-reset
+    set_drv_conf_prop $drv_handle C_I2C_CLK_FREQ_HZ xlnx,clock-freq int
+
 }
