6c3b6bc9f023f6f54cd6354c3f9d61b5d816c166
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2020-03-11 04:01:07 -0600

    Populate nand flash baseaddress to smc node
    This patch updates smc node with nand flash baseaddress and
    bus widht property
    
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- smccps/data/smccps.tcl
+++ smccps/data/smccps.tcl
@@ -13,4 +13,16 @@
 #
 
 proc generate {drv_handle} {
+	foreach i [get_sw_cores device_tree] {
+		set common_tcl_file "[get_property "REPOSITORY" $i]/data/common_proc.tcl"
+		if {[file exists $common_tcl_file]} {
+			source $common_tcl_file
+			break
+		}
+	}
+	set handle [get_cells -hier -filter {IP_NAME==ps7_nand}]
+	set reg [get_baseaddr [get_cells -hier $handle]]
+	hsi::utils::add_new_property $drv_handle "flashbase" int $reg
+	set bus_width [get_property CONFIG.C_NAND_WIDTH [get_cells -hier $handle]]
+	hsi::utils::add_new_property $drv_handle "nand-bus-width" int $bus_width
 }
