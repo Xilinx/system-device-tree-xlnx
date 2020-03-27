Xilinx/system-device-tree-xlnx

commit d55d2091fefa42ab9ca0128ccfb0eda598256fe3

Fix the duplicate register addresses in reg property
In some designs there are multiple duplicate addresses are
generated, so fix this.

Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

device_tree/data/common_proc.tcl
@@ -2097,6 +2097,12 @@ proc gen_reg_property {drv_handle {skip_ps_check ""}} {
 					set reg "$base $size"
 				}
 			} else {
+				if {[string match -nocase $proctype "ps7_cortexa9"] || [string match -nocase $proctype "microblaze"]} {
+					set index [check_base $reg $base $size]
+					if {$index == "true"} {
+						continue
+					}
+				}
 				# ensure no duplication
 				if {![regexp ".*${reg}.*" "$base $size" matched]} {
 					if {[string match -nocase $proctype "psu_cortexa53"] || [string match -nocase $proctype "psu_cortexa72"] || [string match -nocase $proctype "psv_cortexa72"]} {
@@ -2109,6 +2115,58 @@ proc gen_reg_property {drv_handle {skip_ps_check ""}} {
 		}
 	}
 	set_drv_prop_if_empty $drv_handle reg $reg intlist
+}
+
+proc check_base {reg base size} {
+	set len [llength $reg]
+	switch $len {
+		"2" {
+			set base_index0 [lindex $reg 0]
+			set size_index0 [lindex $reg 1]
+			if {$base_index0 == $base || $size_index0 == $size} {
+				return true
+			}
+		}
+		"4" {
+			set base_index0 [lindex $reg 0]
+			set size_index0 [lindex $reg 1]
+			set base_index1 [lindex $reg 2]
+			set size_index1 [lindex $reg 3]
+			if {$base_index0 == $base || $base_index1 == $base} {
+				if {$size_index0 == $size || $size_index1 == $size} {
+					return true
+				}
+			}
+		}
+		"6" {
+			set base_index0 [lindex $reg 0]
+			set size_index0 [lindex $reg 1]
+			set base_index1 [lindex $reg 2]
+			set size_index1 [lindex $reg 3]
+			set base_index2 [lindex $reg 4]
+			set size_index2 [lindex $reg 5]
+			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base} {
+				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size} {
+					return true
+				}
+			}
+		}
+		"8" {
+			set base_index0 [lindex $reg 0]
+			set size_index0 [lindex $reg 1]
+			set base_index1 [lindex $reg 2]
+			set size_index1 [lindex $reg 3]
+			set base_index2 [lindex $reg 4]
+			set size_index2 [lindex $reg 5]
+			set base_index3 [lindex $reg 6]
+			set size_index3 [lindex $reg 7]
+			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base} {
+				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size} {
+					return true
+				}
+			}
+		}
+	}
 }
 
 proc gen_compatible_property {drv_handle} {

commit d53534247e4e47d582785740746f1af69f30b407

Fix multiple base address generation
some IPs like axi_fifo_mm_s has two mappings(AXI4_BASEADDR, BASEADDR).
in this case the reg property should be reg = <base1 size1>, <base2 size2>.
But the existing gen_reg_property proc is not generating this as required.
There is proc check_base, where we are validating duplicate base addresses.
Instead of checking duplication is base address, it is also checking
size duplication. this size duplication is not necessary because size can
be same for different addresses. we have to check only for address duplication.
This patch removes size checking to generate the required reg value.

Fixes: d55d2091fefa ("Fix the duplicate register addresses in reg property")
Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
Acked-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

device_tree/data/common_proc.tcl
@@ -2308,7 +2308,7 @@ proc check_base {reg base size} {
 		"2" {
 			set base_index0 [lindex $reg 0]
 			set size_index0 [lindex $reg 1]
-			if {$base_index0 == $base || $size_index0 == $size} {
+			if {$base_index0 == $base} {
 				return true
 			}
 		}
@@ -2318,9 +2318,7 @@ proc check_base {reg base size} {
 			set base_index1 [lindex $reg 2]
 			set size_index1 [lindex $reg 3]
 			if {$base_index0 == $base || $base_index1 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size} {
 					return true
-				}
 			}
 		}
 		"6" {
@@ -2331,9 +2329,7 @@ proc check_base {reg base size} {
 			set base_index2 [lindex $reg 4]
 			set size_index2 [lindex $reg 5]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size} {
 					return true
-				}
 			}
 		}
 		"8" {
@@ -2346,9 +2342,7 @@ proc check_base {reg base size} {
 			set base_index3 [lindex $reg 6]
 			set size_index3 [lindex $reg 7]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size} {
 					return true
-				}
 			}
 		}
 		"10" {
@@ -2363,9 +2357,7 @@ proc check_base {reg base size} {
 			set base_index4 [lindex $reg 8]
 			set size_index4 [lindex $reg 9]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base || $base_index4 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size || $size_index4 == $size} {
 					return true
-				}
 			}
 		}
 		"12" {
@@ -2382,9 +2374,7 @@ proc check_base {reg base size} {
 			set base_index5 [lindex $reg 10]
 			set size_index5 [lindex $reg 11]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base || $base_index4 == $base || $base_index5 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size || $size_index4 == $size || $size_index5 == $size} {
 					return true
-				}
 			}
 		}
 		"14" {
@@ -2403,9 +2393,7 @@ proc check_base {reg base size} {
 			set base_index6 [lindex $reg 12]
 			set size_index6 [lindex $reg 13]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base || $base_index4 == $base || $base_index5 == $base || $base_index6 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size || $size_index4 == $size || $size_index5 == $size || $size_index6 == $size} {
 					return true
-				}
 			}
 		}
 		"16" {
@@ -2426,9 +2414,7 @@ proc check_base {reg base size} {
 			set base_index7 [lindex $reg 14]
 			set size_index7 [lindex $reg 15]
 			if {$base_index0 == $base || $base_index1 == $base || $base_index2 == $base || $base_index3 == $base || $base_index4 == $base || $base_index5 == $base || $base_index6 == $base || $base_index7 == $base} {
-				if {$size_index0 == $size || $size_index1 == $size || $size_index2 == $size || $size_index3 == $size || $size_index4 == $size || $size_index5 == $size || $size_index6 == $size || $size_index7 == $size} {
 					return true
-				}
 			}
 		}
 	}