6f8ca1a78b5554367132b1a0045921e304b05b67
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2020-09-22 01:50:05 -0600

    ospipsv: Added support for reading OSPI connection mode
    Based on the C_OSPI_MODE parameter, set the "is-stacked"
    and "is-dual" properties and added to OSPI DT node.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
    Acked-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- ospips/data/ospips.tcl
+++ ospips/data/ospips.tcl
@@ -14,7 +14,16 @@
 
 namespace eval ospips {
 proc generate {drv_handle} {
-
+	set node [get_node $drv_handle]
         set_drv_conf_prop $drv_handle C_OSPI_CLK_FREQ_HZ xlnx,clock-freq int
+	set ospi_handle [hsi::get_cells -hier $drv_handle]
+        set ospi_mode [hsi::utils::get_ip_param_value $ospi_handle "C_OSPI_MODE"]
+        set is_stacked 0
+        set is_dual 0
+        if {$ospi_mode == 1} {
+             set is_stacked 1
+        }
+	add_prop $node "is-dual" $is_dual int "pcw.dtsi"
+	add_prop $node "is-stacked" $is_stacked int "pcw.dtsi"
 }
 }
