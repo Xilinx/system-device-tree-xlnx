10d28ffeacb1d5890c2b7ada5ab37bc0b2eb0e52
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2020-10-07 03:34:32 -0600

    Fix incorrect braces
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- hdmi_rx_ss/data/hdmi_rx_ss.tcl
+++ hdmi_rx_ss/data/hdmi_rx_ss.tcl
@@ -139,7 +139,6 @@ namespace import ::tclapp::xilinx::devicetree::common::\*
 	}
 
 	}
-}
 
 proc gen_frmbuf_node {ip drv_handle dts_file} {
    #     set dt_overlay [get_property CONFIG.dt_overlay [get_os]]
@@ -156,4 +155,5 @@ proc gen_frmbuf_node {ip drv_handle dts_file} {
 	add_prop "$vcap_port_node" "direction" input string $dts_file
 	set vcap_in_node [create_node -n "endpoint" -l $ip$drv_handle -p $vcap_port_node -d $dts_file]
 	add_prop "$vcap_in_node" "remote-endpoint" hdmirx_out$drv_handle reference $dts_file
+}
 }
