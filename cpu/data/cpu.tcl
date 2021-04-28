8b68c3fd265681cf23ad955fd1f344d4dc399d8c
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2021-04-28 01:19:50 -0600

    Fix the dcache and icache size and address properties
    When PAE is enabled the size and address range for icache
    and dcache can be 64-bit, add support for the 64-bit.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- cpu/data/cpu.tcl
+++ cpu/data/cpu.tcl
@@ -40,25 +40,31 @@ namespace import ::tclapp::xilinx::devicetree::common::\*
 		add_prop $node "timebase-frequency" $clk int $dts_file
 	}
 	set icache_size [get_ip_param_value $ip "C_CACHE_BYTE_SIZE"]
+	set isize  [check_64bit $icache_size]
 	set icache_base [get_ip_param_value $ip "C_ICACHE_BASEADDR"]
+	set ibase  [check_64bit $icache_base]
 	set icache_high [get_ip_param_value $ip "C_ICACHE_HIGHADDR"]
+	set ihigh_base  [check_64bit $icache_high]
 	set dcache_size [get_ip_param_value $ip "C_DCACHE_BYTE_SIZE"]
+	set dsize  [check_64bit $dcache_size]
 	set dcache_base [get_ip_param_value $ip "C_DCACHE_BASEADDR"]
+	set dbase  [check_64bit $dcache_base]
 	set dcache_high [get_ip_param_value $ip "C_DCACHE_HIGHADDR"]
+	set dhigh_base  [check_64bit $dcache_high]
 	set icache_line_size [expr 4*[get_ip_param_value $ip "C_ICACHE_LINE_LEN"]]
 	set dcache_line_size [expr 4*[get_ip_param_value $ip "C_DCACHE_LINE_LEN"]]
 
 
 	if { [llength $icache_size] != 0 } {
-		add_prop $node "i-cache-baseaddr"  "$icache_base" hexint $dts_file
-		add_prop $node "i-cache-highaddr" $icache_high hexint $dts_file
-		add_prop $node "i-cache-size" $icache_size int $dts_file
+		add_prop $node "i-cache-baseaddr"  "$ibase" hexint $dts_file
+		add_prop $node "i-cache-highaddr" $ihigh_base hexint $dts_file
+		add_prop $node "i-cache-size" $isize int $dts_file
 		add_prop $node "i-cache-line-size" $icache_line_size int $dts_file
 	}
 	if { [llength $dcache_size] != 0 } {
-		add_prop $node "d-cache-baseaddr"  "$dcache_base" hexint $dts_file
-		add_prop $node "d-cache-highaddr" $dcache_high hexint $dts_file
-		add_prop $node "d-cache-size" $dcache_size int $dts_file
+		add_prop $node "d-cache-baseaddr"  "$dbase" hexint $dts_file
+		add_prop $node "d-cache-highaddr" $dhigh_base hexint $dts_file
+		add_prop $node "d-cache-size" $dsize int $dts_file
 		add_prop $node "d-cache-line-size" $dcache_line_size int $dts_file
 	}
 	set model "[get_property IP_NAME $ip],[get_ip_version $ip]"
@@ -70,4 +76,21 @@ namespace import ::tclapp::xilinx::devicetree::common::\*
 	set master_root_node [gen_root_node $drv_handle]
 	set nodes [gen_cpu_nodes $drv_handle]
    }
+
+	proc check_64bit {base} {
+       		if {[regexp -nocase {0x([0-9a-f]{9})} "$base" match]} {
+               		set temp $base
+	               set temp [string trimleft [string trimleft $temp 0] x]
+	               set len [string length $temp]
+	               set rem [expr {${len} - 8}]
+	               set high_base "0x[string range $temp $rem $len]"
+	               set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
+	               set low_base [format 0x%08x $low_base]
+	               set reg "$low_base $high_base"
+	       } else {
+        		set reg "$base"
+       		}
+	       return $reg
+	}
+
 }
