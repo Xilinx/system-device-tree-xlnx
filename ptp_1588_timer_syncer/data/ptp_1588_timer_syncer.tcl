b50eaaebfa066c71bcf2337935e1e9ac6330ad5f
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2021-07-15 23:03:58 -0600

    Add support for the PTP 1588 Timer Syncer IP
    The PTP 1588 Timer Syncer IP provides reference time
    to all the Ethernet ports. It implements an IEEE1588
    real time clock timer and can accurately recreate the 1588
    timer in a local port’s clock domain.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>

---
+++ ptp_1588_timer_syncer/data/ptp_1588_timer_syncer.tcl
@@ -0,0 +1,23 @@
+#
+# (C) Copyright 2021 Xilinx, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+proc generate {drv_handle} {
+	set node [get_node $drv_handle]
+	if {$node == 0} {
+		return
+	}
+	set dts_file [set_drv_def_dts $drv_handle]
+	set keyval [pldt append $node compatible "\ \, \"xlnx,timer-syncer-1588-1.0\""]
+	set_drv_prop $drv_handle compatible "$compatible" stringlist
+}
