b4bbec502284f879867117b5c9c13f0ad346b9c5
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2022-03-09 16:16:21 +0530

    ptp_timer: Update the compatible string based on IP version
    Check the IP version and append the compatible string based
    on the version.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- ptp_1588_timer_syncer/data/ptp_1588_timer_syncer.tcl
+++ ptp_1588_timer_syncer/data/ptp_1588_timer_syncer.tcl
@@ -18,6 +18,19 @@ proc generate {drv_handle} {
 		return
 	}
 	set dts_file [set_drv_def_dts $drv_handle]
-	set keyval [pldt append $node compatible "\ \, \"xlnx,timer-syncer-1588-2.0\""]
+	set ip_ver     [get_comp_ver $drv_handle]
+	if {[string match -nocase $ip_ver "2.0"]} {
+		set keyval [pldt append $node compatible "\ \, \"xlnx,timer-syncer-1588-2.0\""]
+	} else {
+		set keyval [pldt append $node compatible "\ \, \"xlnx,timer-syncer-1588-1.0\""]
+	}
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
 }
+
+proc get_comp_ver {drv_handle} {
+       set slave [hsi get_cells -hier ${drv_handle}]
+       set vlnv [split [hsi get_property VLNV $slave] ":"]
+       set ver [lindex $vlnv 3]
+       return $ver
+}
+
