1550113805bcf7493c39542a0e30cec41cf348cf
Bhavana Jupalli <Bhavana.Jupalli@amd.com>
2023-07-06 22:18:32 +0530

    xdmapcie: data: Update properties to support for SDT in CPM5 design
    Add properties to support in SDT flow.
    
    Signed-off-by: Bhavana Jupalli <Bhavana.Jupalli@amd.com>

--- xdmapcie/data/xdmapcie.tcl
+++ xdmapcie/data/xdmapcie.tcl
@@ -5,7 +5,7 @@ proc xdmapcie_generate {drv_handle} {
         }
 	if {[string match -nocase [get_ip_property $drv_handle IP_NAME] "qdma"]} {
 		pldt append $node compatible "\ \, \"xlnx,qdma-host-3.00\""
-		add_prop $node "xlnx,slcr" 0x90000000 hexlist "pl.dtsi" 1
+		add_prop $node "xlnx,csr-slcr" 0x90000000 hexlist "pl.dtsi" 1
 		add_prop $node "xlnx,num-of-bars" 0x2 hexint "pl.dtsi" 1
 		add_prop $node "xlnx,include-baroffset-reg" 0x1 hexint "pl.dtsi" 1
 		add_prop $node "xlnx,port-type" 1 hexint "pl.dtsi" 1
@@ -16,12 +16,14 @@ proc xdmapcie_generate {drv_handle} {
 		add_prop $node "device_type" "pci" string "pl.dtsi"
 	} elseif {[string match -nocase [get_ip_property $drv_handle IP_NAME] "psv_noc_pcie_1"]} {
 		pcwdt append $node compatible "\ \, \"xlnx,versal-cpm-host-1.00\""
+		pcwdt append $node compatible "\ \, \"xlnx,versal-cpm5-host\""
 		add_prop $node "xlnx,num-of-bars" 0x2 hexint "pcw.dtsi" 1
 		add_prop $node "xlnx,port-type" 1 hexint "pcw.dtsi" 1
 		add_prop $node "#address-cells" 3 int "pcw.dtsi"
 		add_prop $node "#size-cells" 2 int "pcw.dtsi"
 		set ranges "0x02000000 0x00000000 0xe0000000 0x0 0xe0000000 0x00000000 0x10000000>, <0x43000000 0x00000080 0x00000000 0x00000080 0x00000000 0x00000000 0x80000000"
 		add_prop $node "ranges" $ranges hexlist "pcw.dtsi"
+		add_prop $node "xlnx,csr-slcr" fce20000 hexint	"pcw.dtsi" 1
 		add_prop $node "device_type" "pci" string "pcw.dtsi"
         }
 }
