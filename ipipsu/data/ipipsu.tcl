e0ad6e128f3b11d92f819b325788892d917e6de0
Onkar Harsh <onkar.harsh@amd.com>
2023-08-01 21:21:53 -1200

    ipipsu: Replacing the hardcoded "ipi-target-count" with the design generated values
    Number of ipi-targets present within a design is not static and it may vary from design.
    As an example, this number comes as 7 for default zynqmp designs and 11 for SSMU CCI enabled
    zynqmp designs. Therefore, the hard coded values have been removed from zynqmp and versal
    static dtsi files and the logic to generate this value as per the design is being added
    in ipipsu specific tcl. While doing so, the stale code in ipipsu.tcl is also being removed.
    
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- ipipsu/data/ipipsu.tcl
+++ ipipsu/data/ipipsu.tcl
@@ -1,36 +1,5 @@
     proc ipipsu_generate {drv_handle} {
-        if {0} {
-        #foreach i [get_sw_cores device_tree] {
-        #set common_tcl_file "[hsi get_property "REPOSITORY" $i]/data/common_proc.tcl"
-        #if {[file exists $common_tcl_file]} {
-        #    source $common_tcl_file
-        #    break
-        #}
-        #}
-        set proc_type [get_sw_proc_prop IP_NAME]
-        if {[string match -nocase $proc_type "psv_pmc"]} {
-        set cpumap [hsi get_property CONFIG.C_CPU_NAME [get_cells -hier $drv_handle]]
-        if {![string match -nocase $cpumap "PMC"]} {
-            return
-        }
-        }
-        if {[string match -nocase $proc_type "psv_cortexa72"] } {
-        set cpumap [hsi get_property CONFIG.C_CPU_NAME [get_cells -hier $drv_handle]]
-        if {![string match -nocase $cpumap "A72"]} {
-            return
-        }
-        } 
-        if {[string match -nocase $proc_type "psv_cortexr5"] } {
-        set cpumap [hsi get_property CONFIG.C_CPU_NAME [get_cells -hier $drv_handle]]
-        if {![string match -nocase $cpumap "R5_0"] || ![string match -nocase $cpumap "R5_1"]} {
-            return
-        }
-        } else {
-        set default_dts [hsi get_property CONFIG.pcw_dts [get_os]]
-        set node [add_or_get_dt_node -n "&$drv_handle" -d $default_dts]
-        add_new_dts_param "$node" "status" "okay" string
-        }
+        set ipi_list [hsi get_cells -hier -filter {IP_NAME == "psu_ipi" || IP_NAME == "psv_ipi" || IP_NAME == "psx_ipi"}]
+        set node [get_node $drv_handle]
+        add_prop $node "xlnx,ipi-target-count" [llength $ipi_list] int "pcw.dtsi"
     }
-    }
-
-
