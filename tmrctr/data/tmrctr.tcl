313351532e4534cc541372c047da4ddaf52a8332
Paladugu, Siva Durga Prasad <siva.durga.prasad.paladugu@amd.com>
2023-08-13 20:57:21 -1200

    tmrctr: Remove duplicate clock-frequency property generation
    Remove duplicate clock-frequency property generation as it is
    already being handled through gen_dev_ccf_binding proc.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>
    Acked-by: Onkar Harsh <onkar.harsh@amd.com>

--- tmrctr/data/tmrctr.tcl
+++ tmrctr/data/tmrctr.tcl
@@ -2,13 +2,6 @@
          set node [get_node $drv_handle]
          set dts_file [set_drv_def_dts $drv_handle]
          pldt append $node compatible "\ \, \"xlnx,xps-timer-1.00.a\""
-        #adding clock frequency
-        set ip [hsi::get_cells -hier $drv_handle]
-        set clk [hsi::get_pins -of_objects $ip "S_AXI_ACLK"]
-        if {[llength $clk] } {
-        set freq [hsi get_property CLK_FREQ $clk]
-        add_prop $node "clock-frequency" $freq hexint $dts_file
-        }
         set proctype [get_hw_family]
         if {[regexp "microblaze" $proctype match]} {
                  gen_dev_ccf_binding $drv_handle "s_axi_aclk"
