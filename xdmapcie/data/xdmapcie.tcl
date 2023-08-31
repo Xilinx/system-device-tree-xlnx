10d608c1d86d4b85f5b22e2dcdafc35691fde1ef
Bhavana Jupalli <Bhavana.Jupalli@amd.com>
2023-08-30 21:43:07 -1200

    xdmapcie: data : Add required condition check to populate properties based on SLCR hsi property
    Add required condition check to distinguish and populate compatible and csr-slcr
    properties for CPM4 and CPM5 IPs based on CPM_SLCR hsi value.
    
    Signed-off-by: Bhavana Jupalli <Bhavana.Jupalli@amd.com>
    Acked-by: Thippeswamy Havalige <thippeswamy.havalige@amd.com>

--- xdmapcie/data/xdmapcie.tcl
+++ xdmapcie/data/xdmapcie.tcl
@@ -82,15 +82,19 @@ proc xdmapcie_generate {drv_handle} {
 		add_prop $node "#size-cells" 2 int "pl.dtsi"
 		add_prop $node "device_type" "pci" string "pl.dtsi"
 	} elseif {[string match -nocase [get_ip_property $drv_handle IP_NAME] "psv_noc_pcie_1"]} {
-		pcwdt append $node compatible "\ \, \"xlnx,versal-cpm-host-1.00\""
-		pcwdt append $node compatible "\ \, \"xlnx,versal-cpm5-host\""
+		if {[string match -nocase [hsi get_property CONFIG.CPM_SLCR [hsi::get_cells -hier versal_cips_0_pspmc_0_psv_cpm]] "0xfca10000"]} {
+			pcwdt append $node compatible "\ \, \"xlnx,versal-cpm-host-1.00\""
+			add_prop $node "xlnx,csr-slcr" "0x6 00000000" hexlist "pcw.dtsi" 1
+		} else {
+			pcwdt append $node compatible "\ \, \"xlnx,versal-cpm5-host\""
+			add_prop $node "xlnx,csr-slcr" "0xfce20000" hexlist "pcw.dtsi" 1
+		}
 		add_prop $node "xlnx,num-of-bars" 0x2 hexint "pcw.dtsi" 1
 		add_prop $node "xlnx,port-type" 1 hexint "pcw.dtsi" 1
 		add_prop $node "#address-cells" 3 int "pcw.dtsi"
 		add_prop $node "#size-cells" 2 int "pcw.dtsi"
 		set ranges "0x02000000 0x00000000 0xe0000000 0x0 0xe0000000 0x00000000 0x10000000>, <0x43000000 0x00000080 0x00000000 0x00000080 0x00000000 0x00000000 0x80000000"
 		add_prop $node "ranges" $ranges hexlist "pcw.dtsi"
-		add_prop $node "xlnx,csr-slcr" fce20000 hexint	"pcw.dtsi" 1
 		add_prop $node "device_type" "pci" string "pcw.dtsi"
         }
 }
