160e9cba6ee328d65a711ef47017a80279a08fe2
Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
2023-09-19 17:22:08 -1200

    ipipsu: Add the targets dynamically
    Currently the slaves are statically added.
    Read it from the design and update the device tree.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
    Reviewed-by: Onkar Harsh <onkar.harsh@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- ipipsu/data/ipipsu.tcl
+++ ipipsu/data/ipipsu.tcl
@@ -16,5 +16,34 @@
     proc ipipsu_generate {drv_handle} {
         set ipi_list [hsi get_cells -hier -filter {IP_NAME == "psu_ipi" || IP_NAME == "psv_ipi" || IP_NAME == "psx_ipi"}]
         set node [get_node $drv_handle]
+	set node_label [string trimleft $node "&"]
+	set buffer_base [hsi get_property CONFIG.C_BUFFER_BASE $drv_handle]
+	set base [get_baseaddr $drv_handle]
         add_prop $node "xlnx,ipi-target-count" [llength $ipi_list] int "pcw.dtsi"
+	set node_space "_"
+	set src [hsi get_property CONFIG.C_BUFFER_INDEX [hsi get_cells -hier $drv_handle]]
+	set idx 0
+	foreach ipi_slave $ipi_list {
+		set slv_node [create_node -n "child" -l "$node_label$node_space$idx" -u $idx -d "pcw.dtsi" -p $node]
+		set buffer_index [hsi get_property CONFIG.C_BUFFER_INDEX [hsi get_cells -hier $ipi_slave]]
+		if {[string match -nocase $buffer_index "NIL"]} {
+			set buffer_index  0xffff
+		} else {
+			if { ![string match -nocase $src "NIL"]} {
+				set req_base [expr  $buffer_base  + 64 * $buffer_index]
+				add_prop $slv_node "xlnx,ipi-req-msg-buf" $req_base hexint "pcw.dtsi"
+				set res_base [expr  $req_base +  0x20]
+				add_prop $slv_node "xlnx,ipi-rsp-msg-buf" $res_base hexint "pcw.dtsi"
+			}
+		}
+		if {[string_is_empty $buffer_index]} {
+			set buffer_index  0xffff
+		}
+		add_prop $slv_node "xlnx,ipi-buf-index" $buffer_index int "pcw.dtsi"
+		set bit_position [hsi get_property CONFIG.C_BIT_POSITION [hsi get_cells -hier $ipi_slave]]
+		add_prop $slv_node "xlnx,ipi-id" $bit_position int "pcw.dtsi"
+		set bit_mask [expr 1 << $bit_position]
+		add_prop $slv_node "xlnx,ipi-bitmask" $bit_mask int "pcw.dtsi"
+		incr idx
+	}
     }
