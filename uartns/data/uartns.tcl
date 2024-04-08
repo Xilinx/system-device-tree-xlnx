8ec287a7be08d1f25940ad0a5299562c5edc598c
Manikanta Guntupalli <manikanta.guntupalli@amd.com>
2024-04-07 23:58:36 -1200

    uartns: Removed unused clock-frequency device tree property
    Removed unused clock-frequency device tree property to fix linux boot
    hang issue without clk_ignore_unused in bootargs.
    
    Signed-off-by: Manikanta Guntupalli <manikanta.guntupalli@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- uartns/data/uartns.tcl
+++ uartns/data/uartns.tcl
@@ -24,21 +24,15 @@
         set ip [hsi::get_cells -hier $drv_handle]
         set has_xin [get_ip_param_value $ip C_HAS_EXTERNAL_XIN]
         set clock_port "S_AXI_ACLK"
-        if { [string match -nocase "$has_xin" "1"] } {
-        set_drv_conf_prop $drv_handle C_EXTERNAL_XIN_CLK_HZ clock-frequency $node
-        # TODO: update the clock-names and clocks properties and create a
-        # fixed clock node. Currently this is causing any issue as the
-        # driver only uses clock-frequency property
 
-        } else {
         set freq [get_clk_pin_freq $ip "$clock_port"]
         add_prop $node "xlnx,clock-freq" $freq int $dts_file
-        }
 
         set proctype [get_hw_family]
         if {[regexp "microblaze" $proctype match]} {
                  gen_dev_ccf_binding $drv_handle "s_axi_aclk"
         }
+        pldt unset $node "clock-frequency"
     }
 
 
