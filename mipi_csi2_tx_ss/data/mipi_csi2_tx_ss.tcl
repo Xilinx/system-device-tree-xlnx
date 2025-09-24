ff2a212249dcdebf3c035d1631fbffaa6c0b9cbe
Asif Ikbal <asif.ikbal@amd.com>
2025-09-24 11:43:53 +0530

    mipi_csi2_tx_ss: Add subcore absolute address calculation
    Add subcore absolute address calculation to support multiple instance of
    MIPI CSI-2 TX subsystems.
    
    Signed-off-by: Asif Ikbal <asif.ikbal@amd.com>
    Acked-by: Prasad Gutti <prasad.gutti@amd.com>

--- mipi_csi2_tx_ss/data/mipi_csi2_tx_ss.tcl
+++ mipi_csi2_tx_ss/data/mipi_csi2_tx_ss.tcl
@@ -99,6 +99,7 @@ proc csitx2_add_hier_instances {drv_handle} {
 		if {![string_is_empty $ip_handle]} {
 			add_prop "$node" "${ip_prefix}-present" 1 int $dts_file
 			add_prop "$node" "${ip_prefix}-connected" $ip_handle reference $dts_file
+                        update_subcore_absolute_addr $drv_handle $ip_handle $dts_file
 		} else {
 			add_prop "$node" "${ip_prefix}-present" 0 int $dts_file
 		}
