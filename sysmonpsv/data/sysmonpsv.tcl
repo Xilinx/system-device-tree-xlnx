b2c5c1cae88a5b68c86e528f05669cf2d7e82430
Anish Kadamathikuttiyil Karthikeyan Pillai <anish.kadamathikuttiyil-karthikeyan-pillai@amd.com>
2025-09-26 08:56:53 +0100

    sysmonpsv: data: Add AIE thermal monitoring in DT generation
    Update the system device tree to integrate the enhanced thermal monitoring capability for the AIE array.
    The modifications align with the new IIO and thermal driver enhancements, for AIE temperature handling.
    
    The following additions are done to the sysmon node:
     - add child node for aie temperature monitoring
     - add the names of aie sysmon satellites for iio channels
    
    Signed-off-by: Anish Kadamathikuttiyil Karthikeyan Pillai <anish.kadamathikuttiyil-karthikeyan-pillai@amd.com>
    Acked-by: Onkar Harsh <onkar.harsh@amd.com>

--- sysmonpsv/data/sysmonpsv.tcl
+++ sysmonpsv/data/sysmonpsv.tcl
@@ -1,6 +1,6 @@
 #
 # (C) Copyright 2020-2022 Xilinx, Inc.
-# (C) Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -42,6 +42,58 @@
         }
         append numsupplies "/bits/8 <$num_supply_channels>"
         add_prop $node "xlnx,numchannels" $numsupplies noformating $dts_file
+        set part_num [hsi get_property DEVICE [hsi::current_hw_design]]
+        set iochnames "io-channel-names = \"sysmon-temp-channel\""
+        set num_aie_sats 0
+        if {$part_num == "xcvc1902" || $part_num == "xc2ve3858"} {
+            for {set temp_sat_num 0} {$temp_sat_num < 63} {incr temp_sat_num} {
+                set temp_sat "SAT_${temp_sat_num}_DESC"
+                set value [get_ip_property $drv_handle CONFIG.$temp_sat]
+                if {[llength $value] != 0} {
+                    set local_value [string tolower [get_ip_property $drv_handle CONFIG.$temp_sat]]
+                    if {$local_value == "me satellite"} {
+                        set temp_node [create_node -n "temp@$temp_sat_num" -p $node -d $dts_file]
+                        add_prop "$temp_node" "reg" "$temp_sat_num" int $dts_file
+                        incr num_aie_sats
+                        append iochnames ", \"aie-temp-ch$num_aie_sats\""
+                        add_prop "$temp_node" "xlnx,name" "aie-temp-ch$num_aie_sats" string $dts_file
+                        add_prop "$temp_node" "xlnx,aie-temp" noformating $dts_file
+                    }
+                }
+            }
+        }
+
+        proc label_exists {label filename} {
+            set search_pattern "$label"
+            set fh [open $filename r]
+            set content [read $fh]
+            close $fh
+            if {[regexp "\\m$search_pattern\\M" $content]} {
+                return 1
+            }
+            return 0
+        }
+
+        set label "sensor0"
+        set common_file "$path/device_tree/data/config.yaml"
+        set release [get_user_config $common_file -kernel_ver]
+        if {[info exists ::dtsi_fname]} {
+            set filename "$path/device_tree/data/kernel_dtsi/$release/$dtsi_fname"
+            if {[label_exists $label $filename]} {
+                set thermal_sensor [create_node -n "&sensor0" -d "pcw.dtsi" -p root]
+                add_prop $thermal_sensor "$iochnames" stringlist "pcw.dtsi"
+                add_prop $node "#io-channel-cells" 1 int pcw.dtsi
+                set aie_temp_index $num_supply_channels
+                set temp_index [expr {$num_supply_channels + $num_aie_sats}]
+                set ioch "io-channels = <&sysmon0 $temp_index>"
+                if {$num_aie_sats > 0} {
+                    for {} {$aie_temp_index < $temp_index} {incr aie_temp_index} {
+                        append ioch ", <&sysmon0 $aie_temp_index>"
+                    }
+                }
+                add_prop $thermal_sensor "$ioch" noformating "pcw.dtsi"
+            }
+        }
     }
 
 
