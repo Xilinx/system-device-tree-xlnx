b7933783b2950834efa96bc6b0d0b8aa3fd952ad
T, Harini <Harini.T@amd.com>
2026-02-16 08:37:51 +0530

    mailbox: Use SLAVE_INTERFACE for processor connectivity detection
    Modify check_if_connected() to detect processor connectivity based on
    SLAVE_INTERFACE property rather than base address matching. This allows
    proper mailbox node mapping when processors have different address views
    of the same peripheral due to address translation in the interconnect.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    
    Acked-for-series: Onkar Harsh <onkar.harsh@amd.com>

--- mailbox/data/mailbox.tcl
+++ mailbox/data/mailbox.tcl
@@ -157,10 +157,9 @@ proc create_mbox_nodes {drv_handle port_interface port_id intr_val intr_parent} 
 proc check_if_connected {periph port_id port_interface processor} {
 	set is_axi4lite_connected 0
 
-	set baseaddr [common::get_property CONFIG.[format "C_S%d_AXI_BASEADDR" $port_id] $periph]
 	set mem [hsi::get_mem_ranges -of_objects [hsi::get_cells -hier $processor] -filter INSTANCE==$periph]
 
-	# Filter out duplicate entries using base:high as key
+	# Filter out duplicate entries using base:high:slave_intf as key
 	array set seen {}
 	set unique_ranges {}
 
@@ -168,17 +167,20 @@ proc check_if_connected {periph port_id port_interface processor} {
 		set name [hsi::get_property NAME $r]
 		set base [hsi::get_property BASE_VALUE $r]
 		set high [hsi::get_property HIGH_VALUE $r]
-		set key "$base:$high"
+		set slave_intf [hsi::get_property SLAVE_INTERFACE $r]
+		set key "$base:$high:$slave_intf"
 		if {![info exists seen($key)]} {
 			set seen($key) 1
 			lappend unique_ranges $r
 		}
 	}
 
-	# Traverse unique entries to find if connected
+	# Check if processor has any mem_range with the correct SLAVE_INTERFACE (S0_AXI or S1_AXI)
+	# This allows NOC address translation where different processors see different addresses
 	foreach r $unique_ranges {
-		set addrs [hsi::get_property BASE_VALUE $r]
-		if {$addrs == $baseaddr} {
+		set slave_intf [hsi::get_property SLAVE_INTERFACE $r]
+		# Check if this mem_range is for the correct port (S0_AXI or S1_AXI)
+		if {[string match "*S${port_id}_AXI*" $slave_intf]} {
 			set is_axi4lite_connected 1
 			break
 		}
