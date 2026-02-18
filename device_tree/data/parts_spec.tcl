0a7c557d2833805586df94352409744face00bbd
Onkar Harsh <onkar.harsh@amd.com>
2026-02-18 16:20:32 +0530

    device_tree: BOARD: Update custom EIO related DT entries
    Update custom EIO related DT entries with a better nomenclature.
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@am=

--- device_tree/data/parts_spec.tcl
+++ device_tree/data/parts_spec.tcl
@@ -1,5 +1,5 @@
 #
-# (C) Copyright 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+# (C) Copyright 2025 - 2026 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -12,13 +12,14 @@
 # GNU General Public License for more details.
 #
 
-global is_versal_2ve_2vm_seio_platform
-set is_versal_2ve_2vm_seio_platform 0
+global is_versal_2ve_2vm_custom_eio_platform
+set is_versal_2ve_2vm_custom_eio_platform 0
 
 proc part_specific_init_proclist {} {
-	global is_versal_2ve_2vm_seio_platform
-	if {[llength [hsi::get_cells -hier -filter {IP_NAME==seio || IP_NAME==svip}]]} {
-		set is_versal_2ve_2vm_seio_platform 1
+	global is_versal_2ve_2vm_custom_eio_platform
+	set device [hsi get_property DEVICE [hsi::current_hw_design]]
+	if {$device == "xa2ve3288"} {
+		set is_versal_2ve_2vm_custom_eio_platform 1
 		variable ::sdtgen::namespacelist
 		dict set ::sdtgen::namespacelist "seio_spi" "spips"
 		dict set ::sdtgen::namespacelist "seio_gpio" "gpiops"
@@ -27,25 +28,25 @@ proc part_specific_init_proclist {} {
 }
 
 proc part_specific_ps_mapping {def_ps_mapping} {
-	global is_versal_2ve_2vm_seio_platform
-	if {$is_versal_2ve_2vm_seio_platform} {
+	global is_versal_2ve_2vm_custom_eio_platform
+	if {$is_versal_2ve_2vm_custom_eio_platform} {
 		dict unset def_ps_mapping ed000000
-		dict set def_ps_mapping ed010000 label spi0_seio
-		dict set def_ps_mapping ed020000 label spi1_seio
-		dict set def_ps_mapping ed030000 label spi2_seio
-		dict set def_ps_mapping ed040000 label spi3_seio
-		dict set def_ps_mapping ed050000 label serial0_seio
-		dict set def_ps_mapping ed060000 label serial1_seio
-		dict set def_ps_mapping ed070000 label serial2_seio
-		dict set def_ps_mapping ed080000 label gpio_seio
-		dict set def_ps_mapping ed0c0000 label pcie_seio
+		dict set def_ps_mapping ed010000 label spi0_eio
+		dict set def_ps_mapping ed020000 label spi1_eio
+		dict set def_ps_mapping ed030000 label spi2_eio
+		dict set def_ps_mapping ed040000 label spi3_eio
+		dict set def_ps_mapping ed050000 label serial0_eio
+		dict set def_ps_mapping ed060000 label serial1_eio
+		dict set def_ps_mapping ed070000 label serial2_eio
+		dict set def_ps_mapping ed080000 label gpio_eio
+		dict set def_ps_mapping ed0c0000 label pcie_eio
 	}
 	return $def_ps_mapping
 }
 
 proc gen_part_specific_misc {} {
-	global is_versal_2ve_2vm_seio_platform
-	if {$is_versal_2ve_2vm_seio_platform} {
-		update_system_dts_include "versal2-xa2ve3288.dtsi"
+	global is_versal_2ve_2vm_custom_eio_platform
+	if {$is_versal_2ve_2vm_custom_eio_platform} {
+		update_system_dts_include "versal2-custom-eio.dtsi"
 	}
 }
