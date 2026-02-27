b132c4fd24179b666d31599d1e811e02bcc7ab6e
Asif Ikbal <asif.ikbal@amd.com>
2026-02-27 12:57:04 +0530

    framebuf_wr: Add v-frmbuf-wr-v3.0 compatible string
    Append the v-frmbuf-wr-v3.0 compatible string to ensure
    compatibility with Linux driver.
    
    Signed-off-by: Asif Ikbal <asif.ikbal@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- framebuf_wr/data/framebuf_wr.tcl
+++ framebuf_wr/data/framebuf_wr.tcl
@@ -18,7 +18,7 @@
         if {$node == 0} {
                 return
         }
-        pldt append $node compatible "\ \, \"xlnx,axi-frmbuf-wr-v2.2\""
+        pldt append $node compatible "\ \ , \"xlnx,v-frmbuf-wr-v3.0\"\ \, \"xlnx,axi-frmbuf-wr-v2.2\""
         set ip [hsi::get_cells -hier $drv_handle]
         set_drv_conf_prop $drv_handle C_S_AXI_CTRL_ADDR_WIDTH xlnx,s-axi-ctrl-addr-width $node
         set_drv_conf_prop $drv_handle C_S_AXI_CTRL_DATA_WIDTH xlnx,s-axi-ctrl-data-width $node
