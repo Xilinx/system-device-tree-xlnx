964fc2a81d64ee8446dc1a16aa68cfb2a8a41203
Dhriti Sree Rompicharla <DhritiSree.Rompicharla@amd.com>
2026-05-14 15:51:11 +0530

    intc: Update intr-parent check to MBV
    Update the intr-parent check from spartanuplus to MBV
    
    Signed-off-by: Dhriti Sree Rompicharla <DhritiSree.Rompicharla@amd.com>
    Acked-by: Onkar Harsh <onkar.harsh@amd.com>

--- intc/data/intc.tcl
+++ intc/data/intc.tcl
@@ -86,8 +86,7 @@
         set_drv_conf_prop $drv_handle C_IVAR_RESET_VALUE "xlnx,ivar-rst-val" $node
         set_drv_conf_prop $drv_handle C_ADDR_WIDTH "xlnx,addr-width" $node
 
-        set variant [hsi get_property CONFIG.C_FAMILY [hsi get_cells -hier $ip]]
-        if {[string match -nocase $variant "spartanuplus"]} {
+        if {[llength [hsi get_cells -hier -filter IP_NAME==microblaze_riscv]] > 0} {
             add_prop $node "interrupt-parent" "cpu0_intc" reference "pl.dtsi"
         }
     }
